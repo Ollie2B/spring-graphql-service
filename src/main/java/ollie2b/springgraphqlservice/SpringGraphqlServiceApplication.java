14a42c67926b8f167e00f411adfe31a1e31a3d6c
Oliver <oliverklein33@gmail.com>
2023-06-29 20:06:45 -0300

    Initial commit

---
+++ src/main/java/ollie2b/springgraphqlservice/SpringGraphqlServiceApplication.java
@@ -0,0 +1,13 @@
+package ollie2b.springgraphqlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ollie2b/springgraphqlservice/SpringGraphqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ollie2b.springgraphqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
